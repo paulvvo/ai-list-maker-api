f4e5000cd45f98bfe9541e505c261daa499616e0
paulvvo <paulvo@live.ca>
2018-09-26 22:15:45 -0400

    added transaction to register route and finish database structure

--- server.js
+++ server.js
@@ -2,6 +2,12 @@ const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const bcrypt = require("bcrypt");
+const clarifai = require("clarifai");
+
+const clarifaiApp = new Clarifai.App({
+ apiKey: 'd4ccf75959a54619b16a900eab3667e7'
+});
+
 const knex = require('knex')({
   client: 'pg',
   connection: {
@@ -12,6 +18,7 @@ const knex = require('knex')({
   }
 });
 
+
 const app = express();
 
 //Middleware
@@ -19,70 +26,34 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 
-const database = [
-  {
-    name: "Billy",
-    password: "pass",
-    email: "billy@gmail.com",
-    list: [1,2,3,4,5,6]
-  },
-  {
-    name: "Jack",
-    password: "pass",
-    email: "jack@gmail.com",
-    list: [1,2,3,4,5,6]
-  },
-  {
-    name: "Berry",
-    password: "pass",
-    email: "berry@gmail.com",
-    list: [1,2,3,4,5,6]
-  },
-  {
-    name: "yogurt",
-    password: "pass",
-    email: "yogurt@gmail.com",
-    list: [1,2,3,4,5,6]
-  }
-]
 
-
-// console.log("here");
-// knex('logins')
-//   .where({ email: 'fdsa' })
-//   .then(console.log);
-
-
-// const saltRounds = 10;
-// const myPlaintextPassword = 's0/\/\P4$$w0rD';
-// bcrypt.hash(myPlaintextPassword, saltRounds, function(err, hash) {
-//   console.log(hash);
-//   bcrypt.compare(myPlaintextPassword, hash, function(err, res) {
-//     // res == true
-//     if(res){
-//       console.log("yeeboi it worked");
-//     }
-//   });
-// });
 
 app.get("/", function(req,res){
-  res.json(database[0]);
+  res.json("Server Running");
 })
 
 app.post("/signin", (req,res) =>{
   const {email, password} = req.body;
-
-  knex("logins").where({email})
+                        //email:email
+  knex("logins").where({email:email})
   .then(foundUser => {
-    if(foundUser[0]){
-      bcrypt.compare(foundUser[0].hash, password, function(err,results){
+
+
+    if(foundUser[0] && foundUser.length >0){
+      bcrypt.compare(password, foundUser[0].hash, function(err,result){
         if(err){
-          res.status(400).json("Failed to Login, Please Try Again [Code:Compara]")
+          console.log(err);
+          res.status(400).json("Failed to Login, Please Try Again");
+        }else if(result){
+          knex("users")
+          .where({email:email})
+          .then(user => res.json(user[0]));
+        }else {
+          res.status(400).json("Failed to Login, Please Try Again");
         }
-        res.json(foundUser[0]);
       })
     }else{
-      res.status(400).json("Failed to Login, Please Try Again [Code:!Select]");
+      res.status(400).json("Failed to Login, Please Try Again");
     }
   });
 });
@@ -91,20 +62,37 @@ app.post("/register", (req,res) =>{
   const {email, password, name} = req.body;
 
   if(email && password){
+
     bcrypt.hash(password, 10, function(err,hash){
-      console.log(hash);
-      knex("logins").insert({
-        email,
-        hash,
-      })
-      .returning("*")
-      .then(returningUser => {
-          res.json(returningUser[0]);
-      })
-      .catch(err => {
+      // console.log(hash);
+
+
+      knex.transaction(trx => {
+        trx("logins").insert({
+          email,
+          hash,
+        })
+        .returning("email")
+        .then(returningEmail => {
+          return trx("users").insert({
+            name:name,
+            email:returningEmail[0],
+            detecteditems: JSON.stringify([])
+          })
+          .returning("*")
+          .then(returningUser => res.json(returningUser));
+        })
+        .then(trx.commit)
+        .catch(trx.rollback)
+
+      }).catch(err => {
         console.log(err);
         res.status(400).json("Error Registering User, Please Try Again");
       });
+
+
+
+
     });
 
   }else{
@@ -112,9 +100,17 @@ app.post("/register", (req,res) =>{
   }
 });
 
-app.get("/analyze", (req,res) =>{
-  res.json("analyze route");
+app.post("/urlInputAnalyze", (req,res) =>{
+  clarifaiApp.models
+  .predict("aaa03c23b3724a16a56b629203edc62c", req.body.urlInput)
+  .then(response => {
+      res.json(response);
+  });
 })
+
+
+
+
 
 app.listen(3000, () =>{
   console.log("Server is listening");
@@ -125,3 +121,22 @@ app.listen(3000, () =>{
 // register
 // user
 // signout
+// listupdate (add) post
+// listupdate modify put
+//listdeleteitem delete
+
+
+  //
+  // const newOjb = [
+  //     {jack:"yellow"},
+  //     {terry:"red"},
+  //     {cole:"blue"}
+  // ];
+  //
+  // knex("jsontest").insert({
+  //   data:JSON.stringify(newOjb)
+  // })
+  // .catch(err => console.log("There was an error"));
+  //
+  // knex("jsontest")
+  // .then(response => res.json(response));
